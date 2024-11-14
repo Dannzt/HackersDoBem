dcda8e6bba7fab7a0afe4ceedefa9d5015bfb784
Dan <111394311+Dannzt@users.noreply.github.com>
2024-11-14 12:08:17 -0300

    Update Commands_PowerShell.ps1

--- Scripts/Windows/Commands_PowerShell.ps1
+++ Scripts/Windows/Commands_PowerShell.ps1
@@ -2,14 +2,14 @@
   Get-Process Format-Table -AutoSize
 
 #Script para criar um arquivo zip com os arquivos de uma pasta<br>
-  $source = "C:\Users\user\Documents"<br>
-  $destination = "C:\Users\user\Documents.zip"<br>
+  $source = "C:\Users\user\Documents"
+  $destination = "C:\Users\user\Documents.zip"
   Compress-Archive -Path $source -DestinationPath $destination
 
 #Script para enviar um email usando o Outlook<br>
   $Outlook New-Object-Comobject Outlook.Application<br>
-  $Mail $Outlook.CreateItem(0)<br>
-  $Mail.To = "destinatario@email.com"<br>
-  $Mail.Subject = "Assunto do email"<br>
-  $Mail.Body = "Corpo do email"<br>
+  $Mail $Outlook.CreateItem(0)
+  $Mail.To = "destinatario@email.com"
+  $Mail.Subject = "Assunto do email"
+  $Mail.Body = "Corpo do email"
   $Mail.Send()
